c7399737df6db82a340c035ca328e2e21db68f6e
marine-jacquens <marine.jacquens@laplateforme.io>
2020-10-06 12:12:20 +0200

    ajout inscription form

--- inscription.php
+++ inscription.php
@@ -16,7 +16,54 @@
 		<?php include("includes/header.php")?>
 	</header>
 	<main>
-		
+		<?php 
+			if(isset($_POST['submit']))
+			{
+				$user->register(
+					$_POST['lastname'],
+					$_POST['firstname'],
+					$_POST['gender'],
+					$_POST['birthday'],
+					$_POST['phone'],
+					$_POST['mail'],
+					$_POST['password'],
+					$_POST['password_check'],
+				);
+			}
+
+			?>
+
+	
+		<form action="" method="post">
+			<h1>INSCRIPTION</h1>
+			<label for="lastname">Nom de famille</label><br>
+	        <input type="text" name="lastname" placeholder="Entrez votre nom de famille"><br>
+	        <label for="firstname">Prénom</label><br>
+	        <input type="text" name="firstname" placeholder="Entrez votre prénom"><br>
+
+	        <input type="radio" name="gender" id="male" value="male">
+	        <label for="male">Homme</label>
+	        <input type="radio" name="gender" id="female" value="female">
+	        <label for="female">Femme</label>
+	        <input type="radio" name="gender" id="no_gender" value="no_gender">
+	        <label for="no_gender">Non genré</label>
+
+	        <label for="birthday">Date de naissance</label><br>
+	        <input type="date" name="birthday"><br>
+	        <label for="phone">N° de téléphone</label><br>
+	        <input type="text" name="phone" placeholder="0123456789">
+	        <label for="mail">Email</label><br>
+	        <input type="text" name="mail" placeholder="email@email.com">
+	            
+
+	        <label for="password">Mot de passe</label><br>
+	        <input type="password" name="password" placeholder="Entrez votre mot de passe"><br>
+			<label for="password_check">Confirmation mot de passe</label><br>
+	        <input type="password" name="password_check" placeholder="Confirmez votre mot de passe"><br>
+
+	        <input type="submit" name="submit" value="ENREGISTRER">
+		</form>
+
 	</main>
 	<footer>
 		
